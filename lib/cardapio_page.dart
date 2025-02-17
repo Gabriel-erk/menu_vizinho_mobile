2be70dc46d7d7f9c605583c7265f8deefa7b3289
Gabriel Erick <gabrielmmve13@gmail.com>
2025-02-17 19:20:16 -0300

    adicionando categorias e sub no topo da página

--- lib/cardapio_page.dart
+++ lib/cardapio_page.dart
@@ -28,7 +28,7 @@ class _CardapioPageState extends State<CardapioPage> {
     try {
       final response =
           // await http.get(Uri.parse('http://10.56.45.27/public/api/cardapio'));
-          await http.get(Uri.parse('http://192.168.0.10/public/api/cardapio'));
+          await http.get(Uri.parse('http://192.168.0.5/public/api/cardapio'));
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         setState(() {
@@ -47,7 +47,7 @@ class _CardapioPageState extends State<CardapioPage> {
   Future<void> listaBanners() async {
     try {
       final response =
-          await http.get(Uri.parse('http://192.168.0.10/public/api/cardapio'));
+          await http.get(Uri.parse('http://192.168.0.5/public/api/banner'));
       if (response.statusCode == 200) {
         setState(() {
           banners = json.decode(response.body);
@@ -69,7 +69,6 @@ class _CardapioPageState extends State<CardapioPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Row(
-          // mainAxis é usado em Row (agrupa na horizontal, e é tipo o jusify content do flex)
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Icon(
@@ -78,17 +77,11 @@ class _CardapioPageState extends State<CardapioPage> {
             )
           ],
         ),
-        // definindo cor de fundo da appBar (basicamente a navbar daqui)
         backgroundColor: const Color(0xff8c6342),
       ),
-      // coloca as três barrinhas no canto superior esquerdo, permitindo um menu lateral (basicamente um menu lateral)
       drawer: Drawer(
-        // child é um elemento filho de drawer
-        // ListVIew é basicamente uma lista ul do html
         child: ListView(
-          // elementos filhos da nossa lista
           children: const [
-            // drawerHeader é basicamente o cabeçalho do menu lateral, o que aparecerá no topo
             SizedBox(
               height: 100,
               child: DrawerHeader(
@@ -102,12 +95,8 @@ class _CardapioPageState extends State<CardapioPage> {
                         color: Color(0xfff9eed9)),
                   )),
             ),
-            // listTitle para tópico de lista
             ListTile(
-              // leading posiciona do lado esquerdo, e está posicionado um icone no lado esquerdo
               leading: Icon(Icons.login),
-              // este posiciona no lado direito
-              // trailing: Icon(Icons.login),
               title: Text("Login"),
             ),
             ListTile(
@@ -130,6 +119,66 @@ class _CardapioPageState extends State<CardapioPage> {
           ? const Center(child: CircularProgressIndicator())
           : ListView(
               children: [
+                // Listar Categorias e Subcategorias no topo
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        // Exibe as Categorias
+                        ...categorias.map((categoria) {
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Ação para exibir produtos da categoria
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff8c6342),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30)),
+                              ),
+                              child: Text(
+                                categoria['titulo_categoria'],
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                        // Exibe as Subcategorias
+                        ...subCategorias.map((subCategoria) {
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Ação para exibir produtos da subcategoria
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff8c6342),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30)),
+                              ),
+                              child: Text(
+                                subCategoria['titulo_sub_categoria'],
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ],
+                    ),
+                  ),
+                ),
+
                 // Listar Categorias
                 ListView.builder(
                   shrinkWrap: true,
@@ -142,7 +191,6 @@ class _CardapioPageState extends State<CardapioPage> {
                     return Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // Título da Categoria
                         Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Text(
@@ -155,7 +203,6 @@ class _CardapioPageState extends State<CardapioPage> {
                             ),
                           ),
                         ),
-                        // Lista de Produtos da Categoria
                         ListView.builder(
                           shrinkWrap: true,
                           physics: const NeverScrollableScrollPhysics(),
@@ -173,17 +220,13 @@ class _CardapioPageState extends State<CardapioPage> {
                                 );
                               },
                               child: Card(
-                                // alterando o shape para para ter bordas arredondadas
                                 shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      15), // Bordas arredondadas
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                elevation:
-                                    5, // Elevação para sombra (cria uma sombra)
+                                elevation: 5,
                                 margin: const EdgeInsets.all(10.0),
                                 child: Row(
                                   children: [
-                                    // Informações do Produto
                                     Expanded(
                                       child: Padding(
                                         padding: const EdgeInsets.all(15.0),
@@ -227,7 +270,6 @@ class _CardapioPageState extends State<CardapioPage> {
                                                     color: Color(0xff8c6342),
                                                   ),
                                                 ),
-                                                // Ícone de adicionar ao carrinho
                                                 IconButton(
                                                   icon: const Icon(
                                                       Icons
@@ -243,7 +285,6 @@ class _CardapioPageState extends State<CardapioPage> {
                                         ),
                                       ),
                                     ),
-                                    // Imagem do Produto
                                     Padding(
                                       padding:
                                           const EdgeInsets.only(right: 10.0),
@@ -252,143 +293,6 @@ class _CardapioPageState extends State<CardapioPage> {
                                         width: 150,
                                         height: 100,
                                         fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                ),
-
-                // Listar Subcategorias
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: subCategorias.length,
-                  itemBuilder: (context, index) {
-                    final subCategoria = subCategorias[index];
-                    final produtos = subCategoria['produtos'];
-
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Título da Subcategoria
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            subCategoria['titulo_sub_categoria'],
-                            style: const TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff8c6342),
-                            ),
-                          ),
-                        ),
-                        // Lista de Produtos da Subcategoria
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: produtos.length,
-                          itemBuilder: (context, prodIndex) {
-                            final produto = produtos[prodIndex];
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        ProdutoPage(produto: produto),
-                                  ),
-                                );
-                              },
-                              child: Card(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      15), // Bordas arredondadas
-                                ),
-                                elevation: 5, // Elevação para sombra
-                                margin: const EdgeInsets.all(10.0),
-                                child: Row(
-                                  children: [
-                                    // Informações do Produto
-                                    Expanded(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(15.0),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              produto['nome'],
-                                              style: const TextStyle(
-                                                fontFamily: 'Poppins',
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w600,
-                                                color: Color(0xff8c6342),
-                                              ),
-                                            ),
-                                            const SizedBox(height: 8),
-                                            Text(
-                                              produto['descricao'],
-                                              maxLines: 2,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                fontFamily: 'Poppins',
-                                                fontSize: 13,
-                                                fontWeight: FontWeight.bold,
-                                                color: Color(0xffacacac),
-                                              ),
-                                            ),
-                                            const SizedBox(height: 15),
-                                            Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment
-                                                      .spaceBetween,
-                                              children: [
-                                                Text(
-                                                  'R\$ ${double.parse(produto['preco']).toStringAsFixed(2)}',
-                                                  style: const TextStyle(
-                                                    fontFamily: 'Poppins',
-                                                    fontSize: 15,
-                                                    fontWeight: FontWeight.w600,
-                                                    color: Color(0xff8c6342),
-                                                  ),
-                                                ),
-                                                // Ícone de adicionar ao carrinho
-                                                IconButton(
-                                                  icon: const Icon(
-                                                      Icons
-                                                          .shopping_cart_outlined,
-                                                      color: Color(0xff8c6342)),
-                                                  onPressed: () {
-                                                    // Ação do botão
-                                                  },
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    // Imagem do Produto
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(right: 10.0),
-                                      child: ClipRRect(
-                                        // borderRadius: BorderRadius.circular(
-                                        //     10), // Borda arredondada na imagem
-                                        child: Image.network(
-                                          produto['imagem'],
-                                          width: 150,
-                                          height: 100,
-                                          fit: BoxFit.cover,
-                                        ),
                                       ),
                                     ),
                                   ],
